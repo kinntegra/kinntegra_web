bc6cf9c8016a858b92e75040a9c759be9ea85b3e
kinntegra <shashikantv@kinntegra.co.in>
2022-01-03 18:07:47 +0530

    kinntegra update

--- app/Http/Controllers/AppSchedulerController.php
+++ app/Http/Controllers/AppSchedulerController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\MarketService;
 
 class AppSchedulerController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance
+     * @return Void
+     */
+    public function __construct(MarketService $marketService)
+    {
+        $this->middleware('auth');
+        parent::__construct($marketService);
+    }
 }
